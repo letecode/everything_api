918b4484996c7657e2ee42411f9f9e121dc4f29d
Jean Claude Mbiya <46892936+johnmbiya@users.noreply.github.com>
2024-09-06 11:05:25 +0100

    validate login data

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -45,6 +45,15 @@ class AuthController extends BaseController
      */
     public function login(Request $request): JsonResponse
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
             $user = User::auth();
             $success['token'] = $user->createToken('EveryThing')->plainTextToken;
